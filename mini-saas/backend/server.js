274dbe6909c8c46638d49e661d159d1db41e0ecd
Luizingxtz <gabrielluiz.al231@gmail.com>
2026-05-07 12:23:27 -0300

    ajustes_backend

--- mini-saas/backend/server.js
+++ mini-saas/backend/server.js
@@ -7,6 +7,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
 const db = new sqlite3.Database('./database.db');
 
 // 🔥 CRIA TABELA AUTOMATICAMENTE
